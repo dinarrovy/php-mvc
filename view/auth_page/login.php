d6ae95b11c90d2c5e2f8f1602d8963b9d8a80a63
dinarrovy <146179596+dinarrovy@users.noreply.github.com>
2024-05-24 20:18:44 +0700

    Add files via upload

---
+++ view/auth_page/login.php
@@ -0,0 +1,30 @@
+<?php
+require_once'../../model/auth_model/login_model.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../../style.css">
+</head>
+<body>
+    <div class="container">
+        <form class="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+            <h2>Login</h2>
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+            <?php if (isset($error)) { ?>
+                <p><?php echo $error; ?></p>
+            <?php } ?>
+            <p>Belum punya akun? <a href="../auth_page/register.php">Daftar disini</a></p>
+        </form>
+    </div>
+</body>
+</html>
+
+
+
